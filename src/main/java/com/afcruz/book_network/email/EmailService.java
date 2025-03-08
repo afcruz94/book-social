a74c3cd6be94ff6c04c581a4dc4be30d7d8b5840
afcruz <andre_cruz94@hotmail.com>
2025-03-08 19:28:44 +0000

    create email service and template

---
+++ src/main/java/com/afcruz/book_network/email/EmailService.java
@@ -0,0 +1,56 @@
+package com.afcruz.book_network.email;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender mailSender;
+    private final SpringTemplateEngine templateEngine;
+
+    @Async
+    public void sendEmail(String to, String username, EmailTemplateName emailTemplateName,
+                          String confirmationUrl, String activationCode, String subject) throws MessagingException {
+        final String templateName;
+
+        if (emailTemplateName == null)
+            templateName = "confirm-email";
+        else
+            templateName = emailTemplateName.name();
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(
+                mimeMessage, MimeMessageHelper.MULTIPART_MODE_MIXED, StandardCharsets.UTF_8.name()
+        );
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("username", username);
+        properties.put("confirmationUrl", confirmationUrl);
+        properties.put("activationCode", activationCode);
+
+        Context context = new Context();
+        context.setVariables(properties);
+
+        mimeMessageHelper.setFrom("andre_dev94@outlook.com");
+        mimeMessageHelper.setText(to);
+        mimeMessageHelper.setSubject(subject);
+
+        final String template = templateEngine.process(templateName, context);
+
+        mimeMessageHelper.setText(template);
+
+        mailSender.send(mimeMessage);
+    }
+}
