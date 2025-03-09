f5c8cbb69b6a48167135c8a88e1a897dfb529ca4
afcruz <andre_cruz94@hotmail.com>
2025-03-09 12:07:10 +0000

    create content dynamic pageable response

---
+++ src/main/java/com/afcruz/book_network/common/PageResponse.java
@@ -0,0 +1,20 @@
+package com.afcruz.book_network.common;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageResponse<T> {
+    private List<T> content;
+    private int pageNumber;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean isFirst;
+    private boolean isLast;
+}
