be328faf8b2ba27b09dec78a076ce74cb4314b8f
afcruz <andre_cruz94@hotmail.com>
2025-03-09 00:37:47 +0000

    set the correct request body annotation

--- src/main/java/com/afcruz/book_network/auth/AuthenticationController.java
+++ src/main/java/com/afcruz/book_network/auth/AuthenticationController.java
@@ -1,15 +1,11 @@
 package com.afcruz.book_network.auth;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("auth")
